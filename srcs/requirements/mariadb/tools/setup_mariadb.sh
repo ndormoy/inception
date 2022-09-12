a5eca91d9600933f952398bf4743e0bbb6d2c821
Noe Dormoy <noedormoy@MacBook-Pro-de-Noe-2.local>
2022-09-12 16:24:45 +0200

    mariadb sh correction

--- srcs/requirements/mariadb/tools/setup_mariadb.sh
+++ srcs/requirements/mariadb/tools/setup_mariadb.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+##########################################################
+## Lancer en arriere plan mysqld (server) pour pouvoir  ##
+## executer des commandes mysql (client)                ##
+##########################################################
+mysqld &
+
+while ! (mysqladmin ping)
+do
+   sleep 3
+   echo "waiting for mysql ..."
+done
+echo "starting the main script"
+
 mysql --execute "CREATE DATABASE ${MYSQL_DATABASE};"
 #mysql --execute "USE ${MYSQL_DATABASE};"
 mysql --execute "CREATE USER '${MYSQL_ROOT_USER}'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
@@ -9,4 +22,4 @@ mysql --execute "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD
 mysql --execute "GRANT ALL PRIVILEGES ON '${MYSQL_DATABASE}'. * TO '${MYSQL_USER}';"
 mysql --execute "FLUSH PRIVILEGES;"
 
-#exec mysqld --user=mysql
+exec mysqld
