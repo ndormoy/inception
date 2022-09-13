e98b8feb626f7af62dd482904cacd642ea11e4a1
Noe Dormoy <noedormoy@desktop-s3lv3r6.home>
2022-09-13 16:29:38 +0200

    Secure if there is already a database named MYSQL_DATABASE in setup_mariadb.sh

--- srcs/requirements/mariadb/tools/setup_mariadb.sh
+++ srcs/requirements/mariadb/tools/setup_mariadb.sh
@@ -1,28 +1,5 @@
 #!/bin/sh
 
-###############################################################
-## A FAIRE : Il faut verifier si on a deja une base de donne ##
-## si oui, on peut passer directement a la derniere commande ##
-###############################################################
-
-
-##########################################################
-## Lancer en arriere plan mysqld (server) pour pouvoir  ##
-## executer des commandes mysql (client)                ##
-##########################################################
-mysqld &
-
-#################################################
-## On regarde si mysqld (le serveur) est lance ##
-## si oui, on continue le programmme           ##
-#################################################
-while ! (mysqladmin ping)
-do
-   sleep 3
-   echo "waiting for mysql server"
-done
-echo "starting the script"
-
 #############################################
 ## A enlever une fois qu'on ajoute le .env ##
 #############################################
@@ -32,17 +9,46 @@ export MYSQL_ROOT_PASSWORD=rootpouet
 export MYSQL_USER=ndormoy
 export MYSQL_PASSWORD=poulet
 
-##################################################
-## Creation de database et user mysql (mariadb) ##
-##################################################
-mysql --execute "CREATE DATABASE ${MYSQL_DATABASE};"
-mysql --execute "CREATE USER '${MYSQL_ROOT_USER}'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-mysql --execute "GRANT ALL PRIVILEGES ON * . * TO '${MYSQL_ROOT_USER}'@'%';"
-mysql --execute "FLUSH PRIVILEGES;"
-mysql --execute "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql --execute "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}. * TO '${MYSQL_USER}';"
-mysql --execute "FLUSH PRIVILEGES;"
+###############################################################
+## A FAIRE : Il faut verifier si on a deja une base de donne ##
+## si oui, on peut passer directement a la derniere commande ##
+###############################################################
+if mysql "${MYSQL_DATABASE}" >/dev/null 2>&1 </dev/null
+then
+  echo "${MYSQL_DATABASE} exists (and I have permission to access it)"
+else
+  echo "${MYSQL_DATABASE} does not exist (or I do not have permission to access it)"
+  echo "... CREATING ${MYSQL_DATABASE} Database"
+  ##########################################################
+  ## Lancer en arriere plan mysqld (server) pour pouvoir  ##
+  ## executer des commandes mysql (client)                ##
+  ##########################################################
+  mysqld &
+  
+  #################################################
+  ## On regarde si mysqld (le serveur) est lance ##
+  ## si oui, on continue le programmme           ##
+  #################################################
+  while ! (mysqladmin ping)
+  do
+     sleep 3
+     echo "waiting for mysql server"
+  done
+  echo "starting the script"
 
-mysqladmin -u root -pSomePass shutdown
+  ##################################################
+  ## Creation de database et user mysql (mariadb) ##
+  ##################################################
+  mysql --execute "CREATE DATABASE ${MYSQL_DATABASE};"
+  mysql --execute "CREATE USER '${MYSQL_ROOT_USER}'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+  mysql --execute "GRANT ALL PRIVILEGES ON * . * TO '${MYSQL_ROOT_USER}'@'%';"
+  mysql --execute "FLUSH PRIVILEGES;"
+  mysql --execute "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+  mysql --execute "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}. * TO '${MYSQL_USER}';"
+  mysql --execute "FLUSH PRIVILEGES;"
+  
+  mysqladmin -u root -pSomePass shutdown
+
+fi
 
 exec mysqld
