824caeafbff683af85698d5b7fcc0f9480e5e884
Noe Dormoy <noedormoy@desktop-s3lv3r6.home>
2022-09-13 14:47:55 +0200

    Fix on setup_mariadb.sh for line 43

--- srcs/requirements/mariadb/tools/setup_mariadb.sh
+++ srcs/requirements/mariadb/tools/setup_mariadb.sh
@@ -40,7 +40,7 @@ mysql --execute "CREATE USER '${MYSQL_ROOT_USER}'@'%' IDENTIFIED BY '${MYSQL_ROO
 mysql --execute "GRANT ALL PRIVILEGES ON * . * TO '${MYSQL_ROOT_USER}'@'%';"
 mysql --execute "FLUSH PRIVILEGES;"
 mysql --execute "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql --execute "GRANT ALL PRIVILEGES ON '${MYSQL_DATABASE}'. * TO '${MYSQL_USER}';"
+mysql --execute "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}. * TO '${MYSQL_USER}';"
 mysql --execute "FLUSH PRIVILEGES;"
 
 exec mysqld
